977d4153bb6fad4d521333e45634fc6907a097e6
Андрій Полунец <polunec.andrey@gmail.com>
2023-12-17 14:52:34 +0200

    Add to throw methods realisations

--- CSharpApp/3.Database/ClientManager.cs
+++ CSharpApp/3.Database/ClientManager.cs
@@ -121,17 +121,96 @@ namespace _3.Database
 
         public Client GetById(int id)
         {
-            throw new NotImplementedException();
+            string sql = $"SELECT * FROM tblClients WHERE Id = {id};";
+            SqlCommand sqlCommand = _conn.CreateCommand();
+            sqlCommand.CommandText = sql;
+
+            using (SqlDataReader reader = sqlCommand.ExecuteReader())
+            {
+                if (reader.Read())
+                {
+                    Client entity = new Client();
+                    entity.Id = int.Parse(reader["Id"].ToString());
+                    entity.LastName = reader["LastName"].ToString();
+                    entity.FirstName = reader["FirstName"].ToString();
+                    entity.Phone = reader["Phone"].ToString();
+                    entity.DateOfBirth = reader["DateOfBirth"].ToString();
+                    entity.CreatedDate = reader["CreatedDate"].ToString();
+                    entity.Sex = Boolean.Parse(reader["Sex"].ToString());
+                    entity.ProfessionId = int.Parse(reader["ProfessionId"].ToString());
+
+                    return entity;
+                }
+            }
+
+            return null;
         }
 
         public void Delete(Client entity)
         {
-            throw new NotImplementedException();
+            if (entity == null)
+            {
+                throw new ArgumentNullException(nameof(entity), "Entity cannot be null");
+            }
+
+            string sql = $"DELETE FROM tblClients WHERE Id = {entity.Id};";
+            SqlCommand sqlCommand = _conn.CreateCommand();
+            sqlCommand.CommandText = sql;
+
+            int rowsAffected = sqlCommand.ExecuteNonQuery();
+
+            if (rowsAffected > 0)
+            {
+                Console.WriteLine($"Record with Id {entity.Id} has been successfully deleted.");
+            }
+            else
+            {
+                Console.WriteLine($"Record with Id {entity.Id} not found or could not be deleted.");
+            }
         }
 
         public void Update(Client entity)
         {
-            throw new NotImplementedException();
+            if (entity == null)
+            {
+                throw new ArgumentNullException(nameof(entity), "Entity cannot be null");
+            }
+            Console.WriteLine($"Введіть нове ім'я для клієнта {entity.FirstName}:");
+            entity.FirstName = Console.ReadLine();
+            Console.WriteLine($"Введіть нове прізвище для клієнта {entity.LastName}:");
+            entity.LastName = Console.ReadLine();
+            Console.WriteLine($"Введіть новий телефон для клієнта {entity.Phone}:");
+            entity.Phone = Console.ReadLine();
+            Console.WriteLine($"Введіть нову дату народження для клієнта {entity.DateOfBirth} (2004-12-08):");
+            entity.DateOfBirth = Console.ReadLine();
+            Console.WriteLine($"Введіть нову професію для клієнта {entity.ProfessionName}:");
+            foreach (var p in _proffesionManager.GetList())
+            {
+                Console.WriteLine(p);
+            }
+            Console.Write("->_");
+            entity.ProfessionId = int.Parse(Console.ReadLine());
+
+            string sql = $"UPDATE tblClients SET " +
+                         $"FirtsName = N'{entity.FirstName}', " +
+                         $"LastName = N'{entity.LastName}', " +
+                         $"Phone = N'{entity.Phone}', " +
+                         $"DateOfBirth = N'{entity.DateOfBirth}', " +
+                         $"ProfessionId = N'{entity.ProfessionId}', " +
+                         $"WHERE Id  = {entity.Id};";
+            SqlCommand sqlCommand = _conn.CreateCommand();
+            sqlCommand.CommandText = sql;
+
+            int rowsAffected = sqlCommand.ExecuteNonQuery();
+
+            if (rowsAffected > 0)
+            {
+                Console.WriteLine($"Record with Id{entity.Id} has been successfully updated. ");
+            }
+            else
+            {
+                Console.WriteLine($"Record with Id {entity.Id} not found or could not be updated.");
+            }
         }
 
         public void GenerateRandom(int count)
